a7bd7c159e4016b32f4cf8711d21412f3c1b219a
Martín Pravia <marto.pravia@gmail.com>
2024-11-25 13:34:50 +0000

    Agrego la funcion de que agregue el texto  una vez escribo la tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      setTasks([...tasks, e.target.value]);
+      e.target.value = "";
+    }
+  };
+
   return (
     <>
       <div className="row d-flex justify-content-center">
         <div className="col-8">
-          <label
-            for="exampleInputEmail1"
-            className="form-label d-flex justify-content-center align-items-center"
-          >
-            <h1 className="display-3">To Do List</h1>
+          <label className="form-label d-flex justify-content-center align-items-center">
+            <h1 className="display-3 title">To Do List</h1>
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            id="inputText"
+            aria-describedby="InputText"
+            placeholder="Insert here your item to the list"
+            onKeyDown={addTask}
           />
 
           <ul className="list-group">
-            <li className="list-group-item">An item</li>
-			<li className="list-group-item">Quantity items left</li>
+            {tasks.map((task, index) => (
+              <li
+                className="list-group-item d-flex justify-content-between"
+                key={index}
+              >
+                {task}
+                <i className="bi bi-trash"></i>
+              </li>
+            ))}
+
+            <li className="list-group-item">Quantity items left</li>
           </ul>
         </div>
       </div>
