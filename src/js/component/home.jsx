bb49cc0e76f799d054303a198d01a32c990d4b5f
Martín Pravia <marto.pravia@gmail.com>
2024-11-25 12:44:54 +0000

    maquetación html del proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <>
+      <div className="row d-flex justify-content-center">
+        <div className="col-8">
+          <label
+            for="exampleInputEmail1"
+            className="form-label d-flex justify-content-center align-items-center"
+          >
+            <h1 className="display-3">To Do List</h1>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+
+          <ul className="list-group">
+            <li className="list-group-item">An item</li>
+			<li className="list-group-item">Quantity items left</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
